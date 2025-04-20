eba6f9c84c4bff1fdddab50a6128b7cc3e276795
RaphaelZrrr <133098523+RaphaelZrrr@users.noreply.github.com>
2025-04-20 21:01:14 +0200

    Create Marque.java

---
+++ Marque.java
@@ -0,0 +1,32 @@
+package Modele;
+
+public class Marque {
+    private int id;
+    private String nom;
+
+    public Marque(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Marque{id=" + id + ", nom='" + nom + "'}";
+    }
+}
